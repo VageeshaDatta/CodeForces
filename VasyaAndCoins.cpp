ed02bb8672ef52493f58d4aae228a41d78d94b17
G.Vageesha Datta <43772803+VageeshaDatta@users.noreply.github.com>
2022-04-08 07:43:15 +0530

    CodeForces Round 780 (Div. 3), problem (A)

---
+++ VasyaAndCoins.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+   int T, a, b;
+    cin >> T;
+    while(T--)
+    {
+    	cin >> a >> b;
+    	if(a==0)
+    	   cout << 1 << endl;
+    	else
+          cout << (1*a)+(2*b)+1 << endl;
+       
+           
+           
+    
+    }
+	// your code goes here
+	return 0;
+}
